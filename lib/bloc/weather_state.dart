f4a8c1e3adda196f61eb33f3c18364a6ac98bb0a
Derrick <dmwilliamson@gmail.com>
2023-03-02 06:59:28 -0600

    create weather state bloc classes

--- lib/bloc/weather_state.dart
+++ lib/bloc/weather_state.dart
@@ -2,9 +2,15 @@ part of 'weather_bloc.dart';
 
 abstract class WeatherState extends Equatable {
   const WeatherState();
-  
+
   @override
   List<Object> get props => [];
 }
 
 class WeatherInitial extends WeatherState {}
+
+class WeatherLoading extends WeatherState {}
+
+class WeatherLoaded extends WeatherState {}
+
+class WeatherLoadError extends WeatherState {}
