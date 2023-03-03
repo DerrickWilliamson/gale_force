54e75b1c4e8b011f04ed73f2788638feb7d80c91
Derrick <dmwilliamson@gmail.com>
2023-03-03 09:17:39 -0600

    several updates to app ui

--- lib/views/screens/weather_screen.dart
+++ lib/views/screens/weather_screen.dart
@@ -1,24 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:gale_force/bloc/weather_bloc.dart';
 
 class WeatherScreen extends StatelessWidget {
   const WeatherScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: BlocProvider(
-          create: (context) => WeatherBloc(),
-          child: Container(
-            child: const Center(
-              child: Text(
-                'Weather Screen',
-                style: TextStyle(fontSize: 40.0),
-              ),
-            ),
-          ),
+    return const Scaffold(
+      backgroundColor: Colors.deepPurpleAccent,
+      body: Center(
+        child: Text(
+          'Weather Screen',
+          style: TextStyle(fontSize: 40.0, color: Colors.white),
         ),
       ),
     );
