b1440bab60d1e9c092a0d79b743129579751425d
Derrick <dmwilliamson@gmail.com>
2023-03-03 12:58:31 -0600

    removed center widget and added additional constants

--- lib/views/screens/location_screen.dart
+++ lib/views/screens/location_screen.dart
@@ -9,47 +9,45 @@ class LocationScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.deepPurpleAccent,
       body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const SizedBox(height: 20.0),
-              const Icon(
-                Icons.location_pin,
-                size: 150.0,
-                color: Colors.white,
-              ),
-              const SizedBox(height: 40.0),
-              Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: TextFormField(
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Enter a city name',
-                    hintStyle: TextStyle(color: Colors.white),
-                  ),
-                  style: const TextStyle(color: Colors.white, fontSize: 25.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(height: 20.0),
+            const Icon(
+              Icons.location_pin,
+              size: 150.0,
+              color: Colors.white,
+            ),
+            const SizedBox(height: 40.0),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter a city name',
+                  hintStyle: TextStyle(color: Colors.white),
                 ),
+                style: const TextStyle(color: Colors.white, fontSize: 25.0),
               ),
-              const SizedBox(
-                height: 10.0,
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.white),
+                foregroundColor: MaterialStateProperty.all(Colors.black),
               ),
-              ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.white),
-                  foregroundColor: MaterialStateProperty.all(Colors.black),
-                ),
-                onPressed: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => const WeatherScreen()));
-                },
-                child: const Text(
-                  'Search',
-                  style: TextStyle(fontSize: 20.0),
-                ),
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => const WeatherScreen()));
+              },
+              child: const Text(
+                'Search',
+                style: TextStyle(fontSize: 20.0),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
