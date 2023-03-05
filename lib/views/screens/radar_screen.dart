981be6b38f1e670d04f9685478551b915d6094ca
Derrick <dmwilliamson@gmail.com>
2023-03-04 18:37:58 -0600

    added radar screen with elevated button to nav back to weather screen

--- lib/views/screens/radar_screen.dart
+++ lib/views/screens/radar_screen.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:gale_force/views/components/constants.dart';
+import 'package:gale_force/views/screens/weather_screen.dart';
 
 class RadarScreen extends StatelessWidget {
   const RadarScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        backgroundColor: Colors.deepPurpleAccent,
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'Radar Screen',
+                  style: TextStyle(fontSize: 40.0, color: kAppTextColor),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => const WeatherScreen()));
+                  },
+                  child: const Text(
+                    'Go to Weather',
+                    style: TextStyle(fontSize: 30.0, color: kAppTextColor),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
--- lib/views/screens/weather_screen.dart
+++ lib/views/screens/weather_screen.dart
@@ -17,23 +17,22 @@ class WeatherScreen extends StatelessWidget {
               '7-day Forecast',
               style: TextStyle(fontSize: 40.0, color: kAppTextColor),
             ),
-            Row(
-                // children: [
-                //   ListView.separated(
-                //       scrollDirection: Axis.vertical,
-                //       shrinkWrap: true,
-                //       itemCount: 7,
-                //       itemBuilder: (BuildContext context, int index) {
-                //         return ListTile(
-                //           title: Text('Day $index'),
-                //           subtitle: Text('Weather $index'),
-                //         );
-                //       },
-                //       separatorBuilder: (context, index) => const Divider(
-                //             color: Colors.white,
-                //           )),
-                // ],
-                ), // could this be a ListView or GridView?
+            ListView.separated(
+              scrollDirection: Axis.vertical,
+              shrinkWrap: true,
+              itemCount: 7,
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  width: 100.0,
+                  child: Card(
+                    child: Text('Day $index'),
+                  ),
+                );
+              },
+              separatorBuilder: (context, index) => const Divider(
+                color: Colors.white,
+              ),
+            ),
             const Text(
               'Current Conditions in <city>',
               style: TextStyle(fontSize: 40.0, color: kAppTextColor),
@@ -48,34 +47,6 @@ class WeatherScreen extends StatelessWidget {
             ),
           ],
         ),
-        // child: Center(
-        //   child: Column(
-        //     mainAxisAlignment: MainAxisAlignment.center,
-        //     children: [
-        //       const Text(
-        //         'Weather Screen',
-        //         style: TextStyle(fontSize: 40.0, color: Colors.white),
-        //       ),
-        //       const SizedBox(
-        //         height: 10.0,
-        //       ),
-        //       ElevatedButton(
-        //         style: ButtonStyle(
-        //           backgroundColor: MaterialStateProperty.all(Colors.white),
-        //           foregroundColor: MaterialStateProperty.all(Colors.black),
-        //         ),
-        //         onPressed: () {
-        //           Navigator.of(context).push(MaterialPageRoute(
-        //               builder: (context) => const LocationScreen()));
-        //         },
-        //         child: const Text(
-        //           'Go to Location Screen',
-        //           style: TextStyle(fontSize: 20.0),
-        //         ),
-        //       ),
-        //     ],
-        //   ),
-        // ),
       ),
     );
   }
