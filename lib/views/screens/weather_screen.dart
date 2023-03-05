1cbb5d0fde4649e44812e3a2c6ce7317e21e8255
Derrick <dmwilliamson@gmail.com>
2023-03-04 18:52:46 -0600

    added gridview.count for 7 day forecast in weather screen

--- lib/views/screens/weather_screen.dart
+++ lib/views/screens/weather_screen.dart
@@ -13,26 +13,43 @@ class WeatherScreen extends StatelessWidget {
         child: Column(
           children: [
             const SizedBox(height: 10.0),
-            const Text(
-              '7-day Forecast',
-              style: TextStyle(fontSize: 40.0, color: kAppTextColor),
-            ),
-            ListView.separated(
-              scrollDirection: Axis.vertical,
-              shrinkWrap: true,
-              itemCount: 7,
-              itemBuilder: (BuildContext context, int index) {
-                return Container(
-                  width: 100.0,
-                  child: Card(
-                    child: Text('Day $index'),
-                  ),
-                );
-              },
-              separatorBuilder: (context, index) => const Divider(
-                color: Colors.white,
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                '7-day Forecast',
+                style: TextStyle(fontSize: 40.0, color: kAppTextColor),
               ),
             ),
+            Container(
+              height: 200.0,
+              width: 500.0,
+              child: GridView.count(
+                  scrollDirection: Axis.horizontal,
+                  crossAxisCount: 1,
+                  children: List.generate(7, (index) {
+                    return Center(
+                      child: Card(
+                        child: Text('Day $index'),
+                      ),
+                    );
+                  })),
+            ),
+            // ListView.separated(
+            //   scrollDirection: Axis.vertical,
+            //   shrinkWrap: true,
+            //   itemCount: 7,
+            //   itemBuilder: (BuildContext context, int index) {
+            //     return Container(
+            //       width: 100.0,
+            //       child: Card(
+            //         child: Text('Day $index'),
+            //       ),
+            //     );
+            //   },
+            //   separatorBuilder: (context, index) => const Divider(
+            //     color: Colors.white,
+            //   ),
+            // ),
             const Text(
               'Current Conditions in <city>',
               style: TextStyle(fontSize: 40.0, color: kAppTextColor),
