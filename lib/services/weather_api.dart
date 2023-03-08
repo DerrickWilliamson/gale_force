f53406702973bed40d6264b5d282193b5781e639
Derrick <dmwilliamson@gmail.com>
2023-03-07 18:29:31 -0600

    created weather api for weather api .com

---
+++ lib/services/weather_api.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+import 'package:gale_force/models/weather.dart';
+import 'package:http/http.dart' as http;
+
+class WeatherApi {
+  Future<Weather> getWeatherData() async {
+    final uri = Uri.parse(
+        'http://api.weatherapi.com/v1/forecast.json?key=d6935384eb3e4934b72234721230703&q=$city&days=1&aqi=no&alerts=no');
+    final response = await http.get(uri);
+    if (response.statusCode == 200) {
+      final json = jsonDecode(response.body);
+      return Weather.fromJson(json);
+    } else {
+      throw Exception('Failed to load weather data');
+    }
+  }
+
+  String city = 'Oklahoma City';
+}
