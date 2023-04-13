ccba440720a804670eee7e1d431df8fbef28c0d1
Derrick <dmwilliamson@gmail.com>
2023-04-13 15:52:30 -0500

    created weather model class

--- lib/modules/weather/models/weather.dart
+++ lib/modules/weather/models/weather.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:json_annotation/json_annotation.dart';
 
-class Weather extends StatelessWidget {
-  const Weather({super.key});
+part 'weather.g.dart';
 
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
+class Weather {
+  @JsonKey(name: 'name')
+  String cityName;
+  MainWeather mainWeather;
+
+  Weather(this.cityName, this.mainWeather);
+}
+
+@JsonSerializable()
+class MainWeather {
+  @JsonKey(name: 'temp')
+  double temperature;
+  @JsonKey(name: 'feels_like')
+  double feelsLike;
+  @JsonKey(name: 'temp_min')
+  double tempMin;
+  @JsonKey(name: 'temp_max')
+  double tempMax;
+  int humidity;
+
+  MainWeather(this.temperature, this.feelsLike, this.tempMin, this.tempMax,
+      this.humidity);
 }
