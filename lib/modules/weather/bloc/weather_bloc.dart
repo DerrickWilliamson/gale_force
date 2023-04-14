d5ecc6024608e1093e66960d554047476a35945b
Derrick <dmwilliamson@gmail.com>
2023-04-14 08:11:00 -0500

    added properties to weather_bloc

--- lib/modules/weather/bloc/weather_bloc.dart
+++ lib/modules/weather/bloc/weather_bloc.dart
@@ -10,16 +10,16 @@ class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
   WeatherBloc() : super(WeatherInitial()) {
     on<FetchWeather>((event, emit) async {
       emit(WeatherLoading());
-      //! since I'm instantiating the weather model and a repo here,
-      //! does this replace the need for doing it in the UI as I'm currently
-      //! doing in the weather_screen.dart file???
+      // since I'm instantiating the weather model and a repo here,
+      // does this replace the need for doing it in the UI as I'm currently
+      // doing in the weather_screen.dart file???
       WeatherRepo repo = WeatherRepo();
       Weather? weather = await repo.getCurrentWeather();
 
-      //! would this be better as 'response.statuscode == 200' as it is
-      //! complaining about that the weather response will always be true?
-      //! and why is that?  is it because I'm not doing any error handeling
-      //! before this is reaching the bloc???
+      // would this be better as 'response.statuscode == 200' as it is
+      // complaining about that the weather response will always be true?
+      // and why is that?  is it because I'm not doing any error handeling
+      // before this is reaching the bloc???
       if (weather != null) {
         emit(WeatherLoaded(weather));
       } else {
