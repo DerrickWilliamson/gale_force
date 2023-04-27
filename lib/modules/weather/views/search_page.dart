09f78cffb143837446697c91de9ed47468f78c90
Derrick <dmwilliamson@gmail.com>
2023-04-27 13:04:28 -0500

    refactoring search text field

--- lib/modules/weather/views/search_page.dart
+++ lib/modules/weather/views/search_page.dart
@@ -1,46 +1,57 @@
 import 'package:flutter/material.dart';
-import 'package:gale_force/modules/weather/views/weather_screen.dart';
 
-class SearchPage extends StatelessWidget {
+class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
+
+  @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  // controller for the text field to receive value of user input
+  final _textFieldController = TextEditingController();
+
+  String userInputtedCity = '';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(50.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text(
-              'Search for \nLocal Weather',
-              style: TextStyle(
-                fontSize: 50.0,
-                fontWeight: FontWeight.bold,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(
-              height: 50.0,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(50.0),
-              child: TextFormField(
-                decoration: const InputDecoration(
-                  hintText: 'Enter a city',
-                ),
+            TextField(
+              controller: _textFieldController,
+              decoration: InputDecoration(
+                border: const OutlineInputBorder(),
+                hintText: 'Enter a city',
+                hintStyle: const TextStyle(fontSize: 20.0),
+                suffixIcon: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        userInputtedCity = _textFieldController.text;
+                        print(userInputtedCity);
+                      });
+                    },
+                    icon: const Icon(Icons.search, size: 35.0)),
               ),
             ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const WeatherScreen(),
-                  ),
-                );
-              },
-              child: const Text('Search', style: TextStyle(fontSize: 20.0)),
-            ),
+            const SizedBox(height: 15.0),
+            // MaterialButton(
+            //   color: Colors.blue,
+            //   onPressed: () {
+            //     setState(() {
+            //       userInputtedCity = _textFieldController.text;
+            //       print(userInputtedCity);
+            //     });
+            //   },
+            //   child: const Text(
+            //     'Search',
+            //     style: TextStyle(color: Colors.white, fontSize: 20.0),
+            //   ),
+            // ),
           ],
         ),
       ),
