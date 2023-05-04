6bc2d4764e1853066020e92cec027437178304c3
Derrick <dmwilliamson@gmail.com>
2023-05-04 18:08:00 -0500

    refactored UI and logic or order of states being checked and built, removed weather_screen completely

--- lib/modules/weather/bloc/weather_bloc.dart
+++ lib/modules/weather/bloc/weather_bloc.dart
@@ -7,19 +7,12 @@ part 'weather_event.dart';
 part 'weather_state.dart';
 
 class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
-  //! A) API key is good.  I've tried mutliple cities and am getting the JSON response to print in the console
-  //! B) Updated bloc to check if the state is not WeatherLoading before emitting WeatherLoading, it seems to be stuck in WeatherInitial
-  //! C) Once a city is inputted, my WeatherScreen immediately goes to the UnknownError state / widget
-  //!       Using breakpoints, it didn't seem that it even checked the prior if statements for various states.
   WeatherBloc() : super(WeatherInitial()) {
     on<FetchWeather>((event, emit) async {
-      if (state is! WeatherLoading) {
-        emit(WeatherLoading());
-      }
+      if (state is WeatherLoading) return;
 
       WeatherRepo repo = WeatherRepo();
       Weather? weather = await repo.getCurrentWeather(userCity: event.city);
-
       if (weather != null) {
         emit(WeatherLoaded(weather));
       } else {
@@ -28,27 +21,3 @@ class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
     });
   }
 }
-
-// class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
-//   WeatherBloc() : super(WeatherInitial()) {
-//     print('Expected #1:  WeatherInitial state is being emitted');
-//     on<FetchWeather>((event, emit) async {
-//       if (state is WeatherInitial) return;
-
-//       if (state is WeatherLoading) {
-//         print('Expected #2:  fetching weather');
-//         emit(WeatherLoading());
-//       }
-
-//       WeatherRepo repo = WeatherRepo();
-//       Weather? weather = await repo.getCurrentWeather(userCity: event.city);
-//       print(weather == null);
-//       emit(WeatherLoaded(weather!));
-
-//       if (state is WeatherError) {
-//         print('WeatherError state is being emitted');
-//         emit(WeatherError('Error fetching weather'));
-//       }
-//     });
-//   }
-// }
--- lib/modules/weather/views/search_page.dart
+++ lib/modules/weather/views/search_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gale_force/modules/weather/bloc/weather_bloc.dart';
-import 'package:gale_force/modules/weather/views/weather_screen.dart';
+import 'package:gale_force/modules/weather/widgets/weather_success_widget.dart';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({Key? key}) : super(key: key);
@@ -20,59 +20,69 @@ class _SearchPageState extends State<SearchPage> {
     return Scaffold(
       body: BlocProvider(
         create: (context) => WeatherBloc(),
-        child: Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/weather_background.png'),
-              fit: BoxFit.cover,
-            ),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(50.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                const SizedBox(height: 215.0),
-                TextField(
-                  style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 25.0,
-                      fontWeight: FontWeight.bold),
-                  controller: _cityController,
-                  decoration: const InputDecoration(
-                    suffixIconColor: Colors.white,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                    ),
-                    hintText: 'Enter a city',
-                    hintStyle: TextStyle(fontSize: 25.0, color: Colors.white),
-                    prefixIcon: Padding(
-                      padding:
-                          EdgeInsetsDirectional.only(start: 12.0, end: 12.0),
-                      child:
-                          Icon(Icons.search, size: 30.0, color: Colors.white),
-                    ),
-                  ),
+        child:
+            BlocBuilder<WeatherBloc, WeatherState>(builder: (context, state) {
+          if (state is WeatherLoaded) {
+            return WeatherSuccess(currentWeather: state.currentWeather);
+          } else if (state is WeatherError) {
+            return Center(
+              child: Text(
+                state.error,
+                style: const TextStyle(color: Colors.white, fontSize: 25.0),
+              ),
+            );
+          } else if (state is WeatherLoading) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (state is WeatherInitial) {
+            return Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/weather_background.png'),
+                  fit: BoxFit.cover,
                 ),
-                const SizedBox(height: 15.0),
-                BlocBuilder<WeatherBloc, WeatherState>(
-                  builder: (context, state) {
-                    return ElevatedButton(
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(50.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    const SizedBox(height: 215.0),
+                    TextField(
+                      style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 25.0,
+                          fontWeight: FontWeight.bold),
+                      controller: _cityController,
+                      decoration: const InputDecoration(
+                        suffixIconColor: Colors.white,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(10.0),
+                          ),
+                        ),
+                        hintText: 'Enter a city',
+                        hintStyle:
+                            TextStyle(fontSize: 25.0, color: Colors.white),
+                        prefixIcon: Padding(
+                          padding: EdgeInsetsDirectional.only(
+                              start: 12.0, end: 12.0),
+                          child: Icon(
+                            Icons.search,
+                            size: 30.0,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 15.0),
+                    ElevatedButton(
                       onPressed: () {
                         final String city = _cityController.text;
                         context
                             .read<WeatherBloc>()
                             .add(FetchWeather(city: city));
-                        // BlocProvider.of<WeatherBloc>(context)
-                        //     .add(FetchWeather(city: city));
-                        if (state is WeatherInitial) {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const WeatherScreen(),
-                            ),
-                          );
-                        }
                       },
                       style: ElevatedButton.styleFrom(
                         backgroundColor: Colors.white,
@@ -83,16 +93,20 @@ class _SearchPageState extends State<SearchPage> {
                         minimumSize: const Size(200.0, 50.0),
                       ),
                       child: const Text(
-                        'Search',
+                        'search',
                         style: TextStyle(fontSize: 25.0),
                       ),
-                    );
-                  },
+                    ),
+                  ],
                 ),
-              ],
-            ),
-          ),
-        ),
+              ),
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        }),
       ),
     );
   }
