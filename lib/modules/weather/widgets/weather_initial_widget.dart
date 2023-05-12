aa4c8ef12ad35314012acd43bb8607b0eff73c95
Derrick <dmwilliamson@gmail.com>
2023-05-12 10:14:21 -0500

    additional UI updates

--- lib/modules/weather/widgets/weather_initial_widget.dart
+++ lib/modules/weather/widgets/weather_initial_widget.dart
@@ -16,22 +16,30 @@ class _WeatherInitialWidgetState extends State<WeatherInitialWidget> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: const Color.fromRGBO(22, 25, 26, 1),
+      color: Colors.grey[300],
       child: Padding(
         padding: const EdgeInsets.all(50.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
-            const SizedBox(height: 215.0),
+            const SizedBox(height: 150.0),
+            const Text(
+              'Find Local Weather',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 50.0),
             TextField(
               controller: _cityController,
               decoration: InputDecoration(
                 enabledBorder: OutlineInputBorder(
-                  borderSide: const BorderSide(color: Colors.white),
+                  borderSide: const BorderSide(color: Colors.black),
                   borderRadius: BorderRadius.circular(10.0),
                 ),
                 focusedBorder: OutlineInputBorder(
-                  borderSide: const BorderSide(color: Colors.white),
+                  borderSide: const BorderSide(color: Colors.black),
                   borderRadius: BorderRadius.circular(10.0),
                 ),
                 prefixIcon: const Padding(
@@ -39,38 +47,43 @@ class _WeatherInitialWidgetState extends State<WeatherInitialWidget> {
                   child: Icon(
                     Icons.search,
                     size: 30.0,
-                    color: Colors.lightGreen,
+                    color: Colors.black,
                   ),
                 ),
                 hintText: 'Enter a City',
                 hintStyle: const TextStyle(
-                  color: Colors.lightGreen,
+                  color: Color.fromARGB(255, 68, 67, 67),
                   fontSize: 25.0,
                   fontWeight: FontWeight.bold,
                 ),
               ),
               style: const TextStyle(
-                color: Colors.white,
-                fontSize: 25.0,
-                fontWeight: FontWeight.w400,
-              ),
+                  color: Colors.black,
+                  fontSize: 25.0,
+                  fontWeight: FontWeight.bold),
             ),
-            const SizedBox(height: 15.0),
+            const SizedBox(height: 10.0),
             ElevatedButton(
               onPressed: () {
                 final String city = _cityController.text;
                 context.read<WeatherBloc>().add(FetchWeather(city: city));
               },
               style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.grey[300],
+                backgroundColor: Colors.blueGrey,
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(10.0),
                 ),
                 minimumSize: const Size(200.0, 50.0),
               ),
-              child: const Text(
-                'search',
-                style: TextStyle(fontSize: 30.0, color: Colors.black),
+              child: const Padding(
+                padding: EdgeInsets.all(6.0),
+                child: Text(
+                  'Search',
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    color: Colors.white,
+                  ),
+                ),
               ),
             ),
           ],
